7c3d2dbc812ab4c12947b537ee4b2a5d7aa8b91f
bettyboobang <zorasolaryefi@gmail.com>
2026-02-13 15:12:59 +0000

    Task 2 stack

--- pr_2/test_helo.c
+++ pr_2/test_helo.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int global_arr[1000] = {1} ;
+void function() {
+ int loc_arr[1000];
+ int init_loc_arr[1000] = {1};
+}
 
 int main() {
+ function();
  printf("Hello World!!\n");
  return 0;
 }
