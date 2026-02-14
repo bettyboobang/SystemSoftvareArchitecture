7a975d0056c382207f298492a2cdd880e73f525e
bettyboobang <zorasolaryefi@gmail.com>
2026-02-14 08:35:00 +0000

    Task 3 showing segments address

--- memory.c
+++ memory.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int global_init = 42;
+int global_uninit;
 
 int main() {
- int i;
- printf("The stack is near %p/n", &i);
+ int stack_var;
+ void *heap_var = malloc(10);
+ printf("Segments in memory:");
+ printf("text segment: %p\n", (void*)main);
+ printf("data segment: %p\n", (void*)&global_init);
+ printf("bss segment: %p\n", (void*)&global_uninit);
+ printf("heap segment: %p\n",heap_var);
+ printf("stack segment: %p\n", (void*)&stack_var);
+ free(heap_var);
  return 0;
-} 
+}
