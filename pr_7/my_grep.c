cfa494be96432500b9649d70c3fea7d27ae49cd6
bettyboobang <zorasolaryefi@gmail.com>
2026-04-06 17:33:12 +0000

    Task 3

---
+++ pr_7/my_grep.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LINE_LENGTH 2048
+
+void hunt_for_the_magic_word(const char *search_word, const char *filename) {
+ FILE *scroll = fopen(filename, "r");
+ if(scroll == NULL) {
+  perror("Error: The ancient scroll could not be opened");
+  return;
+ }
+ char line_buffer[MAX_LINE_LENGTH];
+ int line_number = 1;
+ int found_something = 0;
+ printf(">>> Initiatinf the hunt for the artifact '%s' in '%s'....<<<\n\n", search_word, filename);
+ while (fgets(line_buffer, sizeof(line_buffer), scroll) != NULL) {
+   if(strstr(line_buffer, search_word)!= NULL) {
+    printf("[Line %d]%s", line_number, line_buffer);
+    found_something = 1;
+   }
+ }
+ if(!found_something) {
+    printf("\n>>>The artifact was not found in this realm. <<<\n");
+  } else {
+     printf("\n>>> Search quest completed successfully! <<<\n");
+  }
+  fclose(scroll);
+}
+int main(int argc, char *argv[]) {
+   if(argc != 3) {
+    printf("Error: Incorrect incantation!\n");
+    printf("Usage: %s root <word_to_find> <filename>\n", argv[0]);
+    printf("Example: %s root /etc/passwd\n", argv[0]);
+    return EXIT_FAILURE;
+}
